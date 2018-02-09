ad74d63fa95c20d1f7812ad264de20dc616c514e
Claire <@>
2018-02-09 16:01:34 -0500

    stuff

--- xv6-public/exec.c
+++ xv6-public/exec.c
@@ -40,10 +40,14 @@ exec(char *path, char **argv)
 
 
   // Load program into memory.
-  // todo: add guard page
   sz = 0;
+  
+  // guard page
+  if((sz = allocuvm(pgdir, sz, sz + PGSIZE)) == 0)
+    goto bad;
+  clearpteu(pgdir, (char*)(sz - PGSIZE));
+  
   for(i=0, off=elf.phoff; i<elf.phnum; i++, off+=sizeof(ph)){
-	  cprintf("for loop ran"
     if(readi(ip, (char*)&ph, off, sizeof(ph)) != sizeof(ph)) 
       goto bad;
     if(ph.type != ELF_PROG_LOAD)
