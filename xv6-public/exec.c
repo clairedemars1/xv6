60c966b8fa66487c14e193e7eeda710653183aef
Claire <@>
2018-02-09 16:08:24 -0500

    dereferencing null ptr causes trap 14!

--- xv6-public/exec.c
+++ xv6-public/exec.c
@@ -40,6 +40,11 @@ exec(char *path, char **argv)
 
   // Load program into memory.
   sz = 0;
+  // guard page
+  if((sz = allocuvm(pgdir, sz, sz + PGSIZE)) == 0)
+    goto bad;
+  clearpteu(pgdir, (char*)(sz - PGSIZE));
+  
   for(i=0, off=elf.phoff; i<elf.phnum; i++, off+=sizeof(ph)){
     if(readi(ip, (char*)&ph, off, sizeof(ph)) != sizeof(ph))
       goto bad;
