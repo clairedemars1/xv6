9770701ce62b7913f47389e2fb5ce7cd134afc8b
Claire <@>
2018-03-05 12:16:36 -0500

    changed Makefile: 4 cpus, use kvm

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -243,20 +243,20 @@ growproc(int n)
   uint sz;
   struct proc *curproc = myproc();
   
-  acquire(&ptable.all_heaps_lock);
+  //~ acquire(&ptable.all_heaps_lock);
   //~ release(&ptable.all_heaps_lock); // no problem if i do them righ tafter each other
   
   sz = curproc->sz;
   if(n > 0){
     if((sz = allocuvm(curproc->pgdir, sz, sz + n)) == 0)
-	  release(&ptable.all_heaps_lock);
+	  //~ release(&ptable.all_heaps_lock);
       return -1;
   } else if(n < 0){
     if((sz = deallocuvm(curproc->pgdir, sz, sz + n)) == 0)
-      release(&ptable.all_heaps_lock);
+      //~ release(&ptable.all_heaps_lock);
       return -1;
   }
-  release(&ptable.all_heaps_lock);
+  //~ release(&ptable.all_heaps_lock);
   curproc->sz = sz;
   switchuvm(curproc);
   return 0;
